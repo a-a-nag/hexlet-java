12a3aac8507552695968f1c95fff76ece9fb7559
a-a-nag <xavoqb@gmail.com>
2023-10-13 21:19:59 +0300

    testing dependencies

---
+++ test_depend/Example.java
@@ -0,0 +1,9 @@
+package io.hexlet;
+
+import org.apache.commons.lang3.*;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.reverse("Hexlet"));
+    }
+}
